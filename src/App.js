3c52e0ad369b5ac73182d2a8985d8f0d835835e7
DmitryKro <dmitry.kropachev@functor.team>
2024-01-08 17:41:17 +0300

    added promo cards and input

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function RenderProgress() {
         {ProgressBar()}
       </Columns.Column>
       <Columns.Column>
-        <Card className="card">
+        <Card className="card noLeftBottomRadius">
           <p>
             Но вообще-то вряд ли получится потому что…
           </p>
@@ -46,7 +46,7 @@ function RenderProgress() {
             Узнать наиболее вероятные проблемы, которые у тебя возникнут по мнению нашего ИИ
           </p>
           <div className="centerContentResponsive">
-            <Button color="black" className="buttonNoLeftRadius">
+            <Button color="black" className="noLeftBottomRadius">
               Узнать  
             </Button>
           </div>
@@ -75,7 +75,7 @@ function RenderIdeaInput() {
           </input>
         </Columns.Column>
         <Columns.Column size={3}>
-          <Button color="info"  className="buttonNoLeftRadius">
+          <Button color="info"  className="noLeftBottomRadius">
             Оценить
           </Button>
         </Columns.Column>
@@ -84,12 +84,92 @@ function RenderIdeaInput() {
   );
 }
 
+function RenderCard(text, crossedPrices, actualPrices) {
+  return (
+    <Card className = "noLeftBottomRadius">
+      <p>
+        {text}
+      </p>
+      <div className="spaceBetweenContent">
+        <s>
+          {crossedPrices}
+        </s>
+        <span>
+          {actualPrices}
+        </span>
+      </div>
+    </Card>
+  );
+}
+
+function RenderCards() {
+  const [emailValue, setEmailValue] = useState("");
+  const emailPlaceholder = emailValue === "" ? "" : "Напиши свою почту, а мы пришлем результаты обработки твоей идеи нашим ИИ:";
+  const promoText = [
+    "Набор идей для бизнеса сгенерированных специально для тебя",
+    "Проблемное интервью с потенциальными клиентами",
+    "Подбор сотрудников для твоего бизнеса",
+    "Разработка детального бизнес плана с помощью ИИ",
+    "Генерация рекламных материалов с помощью ИИ"
+  ];
+  const crossedPrices = [
+    "399",
+    "8999",
+    "1299",
+    "14699",
+    "6666"
+  ];
+  const actualPrices = [
+    "199",
+    "3899",
+    "1199",
+    "11999",
+    "4599"
+  ];
+
+  const renderedCards = promoText.map((text, index) => (
+    <Columns.Column key={index} size={2}>
+      {RenderCard(text, crossedPrices[index], actualPrices[index])}
+    </Columns.Column>
+  ));
+
+  return (
+    <div>
+      <span>
+        Выбери промокод на какой из наших продуктов тебе прислать:
+      </span>
+      <Columns>
+        {renderedCards}
+      </Columns>
+      <Columns>
+        <p>
+          {emailPlaceholder}
+        </p>
+        <Columns.Column>
+          <input 
+            className="input is-black" 
+            placeholder={"Напиши свою почту, а мы пришлем результаты обработки твоей идеи нашим ИИ:"}
+            size={9} 
+            value={emailValue} 
+            onChange={(e) => setEmailValue(e.target.value)}
+          />
+        </Columns.Column>
+        <Columns.Column size={3}>
+          <Button color="primary" className="noLeftBottomRadius">
+            Прислать
+          </Button>
+        </Columns.Column>
+      </Columns>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Box>
       {RenderProgress()}
       {RenderIdeaInput()}
+      {RenderCards()}
     </Box>
   );
 }
