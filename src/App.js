ada60a9f4e9e74c80479a2e092162627d1ecc9b6
Ekaterina Shreyner <kkkatya88@yandex.ru>
2024-01-08 15:38:41 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import "bulma/css/bulma.min.css";
+import { useState } from "react";
+import { Button, Form, Icon, Box } from "react-bulma-components";
+import Letter from './letter.svg';
+// const { Input, Field, Control, Label } = Form;
 
 function App() {
+  const [isValue, setIsValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <Button className="test button is-warning is-light">
+        My Bulma button
+      </Button>
+      <Form.Field>
+        <Form.Label>Username</Form.Label>
+        <Form.Control>
+          <Form.Input
+            class="success input is-rounded"
+            placeholder="введите текст"
+            value={isValue}
+            onChange={(e) => setIsValue(e.target.value)}
+          />
+          <Icon align="left">
+            <i className="fas fa-info-circle"></i>
+          </Icon>
+          <Icon
+            align="right"
+            color="info"
+            size="large"
+            // style={{
+            //   border: '1px solid red'
+            // }}
+            className="image"
+          >
+            <i className="fas fa-home fa-2x" />
+          </Icon>
+        </Form.Control>
+        {isValue === "123" && <Form.Help color="danger">This username is available: {isValue}</Form.Help>}
+      </Form.Field>
+    </Box>
   );
 }
 
