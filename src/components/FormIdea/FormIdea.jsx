7403872c597375270ca824c5832f5c8d86e6627f
DmitryKro <dmitry.kropachev@functor.team>
2024-01-19 17:25:21 +0300

    email validation

--- src/components/FormIdea/FormIdea.jsx
+++ src/components/FormIdea/FormIdea.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import './FormIdea.css';
 import { Button, Form } from "react-bulma-components";
@@ -6,12 +6,15 @@ import { Button, Form } from "react-bulma-components";
 function FormIdea(props) {
   const location = useLocation();
   const currentPath = location.pathname;
+  const [isValidEmail, setIsValidEmail] = useState(true);
 
   function onCheckIdea(e) {
     props.onCheckIdea(e)
   }
   function onChangeInput(e) {
     props.onChangeInput(e)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsValidEmail(emailRegex.test(props.value));
   }
 
   return (
@@ -21,20 +24,24 @@ function FormIdea(props) {
           onChange={onChangeInput}
           value={props.value}
           className="main__input"
+          style={!isValidEmail ? { borderBottomColor: 'red' } : {}}
           size={9}
           minLength={2}
           placeholder={props.placeholder}
         />
-        {/* <Form.Help color="danger">This email is invalid</Form.Help> */}
+        {(currentPath !== '/' && !isValidEmail) && 
+        <Form.Help color="danger" className="is-hidden-desktop">Невалидная почта</Form.Help>}
         <Button
           className="main__button-submit"
           type="submit"
-          disabled={props.value === "" ? true : false}
+          disabled={props.value === "" || !isValidEmail ? true : false}
           onClick={onCheckIdea}
         >
           {currentPath === '/'? "Оценить" : "Прислать"}
         </Button>
       </Form.Field>
+      {(currentPath !== '/' && !isValidEmail) && 
+        <Form.Help color="danger" className="is-hidden-mobile">Невалидная почта</Form.Help>}
     </form>
   )
 }
